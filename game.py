d3ab78c5c29dd036ed3216a8f0e86ad7349ddc5c
CalebSim2 <cs7925484@gmail.com>
2026-09-09 09:56:44 -0500

    Update game.py
    Customize arena introduction

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Circle"
+ARENA_NAME = "The Titanium Castle"
 
 
 def main():
@@ -10,10 +10,10 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("Heatseeker")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    print("But no hero has answered the call... yet.")
+    print("But no hero has braved the squable...until")
 
 
 if __name__ == "__main__":
